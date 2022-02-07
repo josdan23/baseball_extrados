db7708fba15448514bc4371f6232b8c34fdb8c54
Daniel Yapura <danielyapura@gmail.com>
2022-02-07 08:49:26 -0300

    actualiza el estado al tocar items del bottomnavbar

--- lib/presentation/widgets/botton_nav_bar_widget.dart
+++ lib/presentation/widgets/botton_nav_bar_widget.dart
@@ -2,28 +2,48 @@
 
 import 'package:flutter/material.dart';
 
-class BottonNavBarWidget extends StatelessWidget {
+
+class BottonNavBarWidget extends StatefulWidget {
 
 
   const BottonNavBarWidget({Key? key}) : super(key: key);
 
   @override
+  State<BottonNavBarWidget> createState() => _BottonNavBarWidgetState();
+}
+
+class _BottonNavBarWidgetState extends State<BottonNavBarWidget> {
+
+  int _currentIndex  = 0;
+
+  @override
   Widget build(BuildContext context) {
 
     return BottomNavigationBar(
-      currentIndex: 1,
-
-      items: const <BottomNavigationBarItem> [
-
+  
+      items: const <BottomNavigationBarItem> [  
         BottomNavigationBarItem(
           icon: _IconNavBar( icon: Icons.collections,),
           label: 'Mi colección'),
-
+    
         BottomNavigationBarItem(
           icon: _IconNavBar(icon: Icons.person_outline), 
           label: 'Mi perfil'),
-
       ],
+
+      currentIndex: _currentIndex,
+
+      onTap: ( index ) {
+
+        setState(() {
+          _currentIndex = index;
+        });
+
+        //TODO: CAMBIAR DE PAGINA
+
+      },
+
+      
     );
   }
 }
